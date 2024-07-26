e4aec30c2528af3ec5d6b4e5d99a45543c350c39
Turkiano <turki.saeed.ksa@gmail.com>
2024-07-26 21:39:57 +0300

    Http method Get/findAll

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 
+using Coffee_Shop_App.src.Databases;
+using Coffee_Shop_App.src.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Coffee_Shop_App.src.Controller;
@@ -10,8 +12,22 @@ namespace Coffee_Shop_App.src.Controller;
 public class UserController : BaseController //the inheritance to get the routing
 {
 
+    private List<User>? _users; //this to get users info as a list
+
+    public UserController() //constructor to get users data from DB
+    {
+
+        _users = new DatabaseContext().users; // new obj database to get users' list
+
+    }
 
 
-
+    [HttpGet] //import the ASP.NetCore package
+    public List<User>? findAll()
+    {
+        return _users; //access to users's data
+    }
 
 }
+
+
