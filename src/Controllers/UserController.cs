5c21034703861d0ac205547798c77b980f0bfd64
Turkiano <turki.saeed.ksa@gmail.com>
2024-07-27 10:09:52 +0300

    findOne method

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -6,9 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace Coffee_Shop_App.src.Controller;
 
 
-
-
-
 public class UserController : BaseController //the inheritance to get the routing
 {
 
@@ -30,11 +27,20 @@ public class UserController : BaseController //the inheritance to get the routin
 
 
     [HttpPost] //POST, PUT, or PATCH use fromBody
-    public User CreateOne([FromBody] User user) 
+    public List<User> CreateOne([FromBody] User user)
     {
         Console.WriteLine($"This is the new data {user.FirstName}"); //this is to test the method
+        _users?.Add(user); //to save data in temporary
+        return _users; //to send data to User list
+    }
+
+
+    [HttpGet("{userId}")] 
+    public User? findOne(string userId)
+    {
+        User? user = _users?.FirstOrDefault( user => user.Id == userId);
         
-        return user; //to send data to User list
+        return user; 
     }
 
 }
